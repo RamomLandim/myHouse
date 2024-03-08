8e80f28a9aeca2207840562474f614c46f0749f1
Ramom Landim <ramomlandim@gmail.com>
2024-03-07 22:53:54 -0300

    Corrigido botao de celular, acrescentado submenus e corrigido alguns produtos

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+$('.produtos').click(function () {
+    $('.menu ul .tipos-produtos').toggleClass('mostra');
+    $('.menu ul .tipos-produtos').removeClass('fecha');
+    $('.menu ul .seta').toggleClass('gira');
+});
+
+function desabilitarCheckbox() {
+    var minhaCheckbox = document.getElementById('close-menu');
+    var meusLinks = document.getElementsByClassName('itens-menu');
+
+    for (var i = 0; i < meusLinks.length; i++) {
+        meusLinks[i].addEventListener('click', function () {
+            minhaCheckbox.checked = false;
+        });
+    }
+
+}
+
+function dasabilitarSubMenu() {
+    var subMenu = document.getElementsByClassName('itens-sub-menu');
+
+    for (var i = 0; i < subMenu.length; i++) {
+        subMenu[i].addEventListener('click', function () {
+            $('.menu ul .tipos-produtos').toggleClass('fecha');
+            $('.menu ul .tipos-produtos').removeClass('mostra');
+        });
+    }
+
+}
+
+desabilitarCheckbox();
+dasabilitarSubMenu();
